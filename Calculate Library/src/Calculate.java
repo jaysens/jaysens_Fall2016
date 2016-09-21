98cdaf1228dbc60f8933dacbad27cd0915171ad3
jaysens <17shij12@millshighsch.org>
2016-09-20 17:33:15 -0700

    Calculate Library
    cool

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -134,7 +134,7 @@ public class Calculate {
 		return true;
 	}
 	
-	public static int gcf(int num1, int num2){
+	public static int gcf(int num1, int num2){	
 		while(num2 != 0){
 			int rem = num1 % num2;
 			num1 = num2;

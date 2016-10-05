f97717387404847163eef554f6ac2ad02726c8f1
jaysens <17shij12@millshighsch.org>
2016-10-05 02:48:33 -0700

    Calculate Library
    cool

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -1,46 +1,60 @@
 
+//@Jaysen Shi
+//Math Library
+//09/19/16
 public class Calculate {
 	
+	//Squares an integer
 	public static int square(int num){
 		return num * num;
 	}
 	
+	//Cubes an integer
 	public static int cube(int num){
 		return num * num * num;
 	}
 	
+	//Returns the average of 2 numbers
 	public static double average(double num1, double num2){
 		return (num1 + num2)/2;
 	}
 	
+	//Returns the average of 3 numbers
 	public static double average(double num1, double num2, double num3){
 		return (num1 + num2 + num3)/3;
 	}
 	
+	//Converts radians to degrees
 	public static double toDegrees(double angle){
 		return (angle * 180)/3.14159;
 	}
 	
+	//Converts degrees to radians
 	public static double toRadians(double angle){
 		return (angle * 3.14159)/180;
 	}
 	
+	//Calculates the discriminant of a quadratic equation
 	public static double discriminant(double a, double b, double c){
 		return (b * b) - (4 * a * c);
 	}
 	
+	//Converts a mixed number into an improper fraction
 	public static String toImproperFrac(int whole, int num, int den){
 		return ((whole * den) + num) + "/" + den;
 	}
 	
+	//Converts an improper fraction into a mixed number
 	public static String toMixedNum(int num, int den){
 		return (num/den) + "_" + (num % den) + "/" + den;
 	}
 	
+	//Converts a binomial multiplication into a quadratic equation
 	public static String foil(int a, int b, int c, int d, String var){
 		return (a * c) + var + "^2 + " + ((a * d) + (b * c)) + var + " + " + (b * d);
 	}
 	
+	//Determines whether or not one integer is evenly divisible by another
 	public static boolean isDivisibleBy(int num, int den){
 		if(den == 0){
 			throw new IllegalArgumentException("Error: divide by 0");
@@ -48,6 +62,7 @@ public class Calculate {
 		return (num % den == 0);
 	}
 	
+	//Returns the absolute value of a number
 	public static double absValue(double num){
 		if(num < 0){
 			return num * -1;
@@ -55,7 +70,8 @@ public class Calculate {
 			return num;
 		}
 	}
-
+	
+	//Returns the larger of the values passed
 	public static int max(int num1, int num2){
 		if(num1 > num2){
 			return num1;
@@ -64,6 +80,7 @@ public class Calculate {
 		}
 	}
 	
+	//Returns the largest of the values passed
 	public static double max(double num1, double num2, double num3){
 		double max;
 		if(num1 > num2){
@@ -78,6 +95,7 @@ public class Calculate {
 		}
 	}
 	
+	//Returns the smaller of the values passed
 	public static int min(int num1, int num2){
 		if(num1 > num2){
 			return num2;
@@ -86,6 +104,7 @@ public class Calculate {
 		}
 	}
 	
+	//Rounds a number to two decimal places
 	public static double round2(double num){
 		num = (int) (num * 1000);
 		
@@ -101,6 +120,7 @@ public class Calculate {
 		return (num /= 100.0);
 	}
 	
+	//Raises a value to a positive integer power
 	public static double exponent(double num, int exp){
 		double cur = 1;
 		for(int i = 0; i < exp; i++){
@@ -109,6 +129,7 @@ public class Calculate {
 		return cur;
 	}
 	
+	//Returns the factorial of an integer
 	public static int factorial(int num){
 		if(num < 0){
 			throw new IllegalArgumentException("Error: negative number");
@@ -121,6 +142,7 @@ public class Calculate {
 		return factorial;
 	}
 	
+	//Determines whether or not an integer is prime
 	public static boolean isPrime(int num){
 		if(num < 2){
 			throw new IllegalArgumentException("Error: Must be greater than 1");
@@ -134,6 +156,7 @@ public class Calculate {
 		return true;
 	}
 	
+	//Returns the greatest common factor of two integers
 	public static int gcf(int num1, int num2){	
 		while(num2 != 0){
 			int rem = num1 % num2;
@@ -144,6 +167,7 @@ public class Calculate {
 		return num1;
 	}
 	
+	//Returns an approximation of the square root of a number, accurate to two decimal places
 	public static double sqrt(double num){
 		if(num < 0){
 			throw new IllegalArgumentException("Error: negative number");
@@ -160,7 +184,7 @@ public class Calculate {
 		return round2(squareRoot);
 	}
 	
-	
+	//Approximates the roots of a quadratic equation
 	public static String quadForm(int a, int b, int c){
 		double disc = discriminant(a, b, c);
 		
